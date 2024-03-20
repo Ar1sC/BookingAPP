c8bf9ead635d7211c0ed11ec0207e48ec03281ad
Aris <Ar1sC@users.noreply.github.com>
2024-03-20 17:18:07 +0200

    Limiti 10K 2#

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Lists from "./pages/list/Lists";
+import Hotel from "./pages/hotel/Hotel";
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}>/</Route>
+        <Route path="/hotels" element={<Lists/>}>/</Route>
+        <Route path="/hotels/:id" element={<Hotel/>}>/</Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
